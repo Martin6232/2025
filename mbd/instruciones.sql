37cfea9642ff8725564b52c1f37d18d82e96c130
martin6232 <martinrodriguez@liceovvh.cl>
2025-03-07 20:42:35 -0300

    ok

--- mbd/instruciones.sql
+++ mbd/instruciones.sql
@@ -1,7 +1,33 @@
-select * from citas;
+/*busqueda simple*/
+select * from citas
+where fecha_cita > '2025-03-14';
+
+select * from consultas
+where motivo = "fiebre alta";
+
+select * from medicos
+where apellido = "Ramirez";
+/*update*/
+update citas
+set fecha_cita = '2025-06-08'
+where idcitas = 4;
+
+select * from citas
+where idcitas = 4;
+
+update usuarios
+set password = "parangarecutirimicuaro"
+where idusuario = 1;
+
+select * from usuarios
+where idusuario = 1;
+/*delete*/
+delete from consultas
+where idconsultas = 1;
+
 select * from consultas;
-select * from especialidad;
-select * from medicos;
-select * from personas;
-select * from tipo_usuario;
-select * from usuarios;
+
+
+
+
+
